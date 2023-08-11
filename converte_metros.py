cd64f64d36e1ee2f3be56a22c8d16036b1c127a4
Leonardo Raimundo <90846755+Leonardo-Raimundo@users.noreply.github.com>
2023-08-11 13:55:33 -0300

    Add files via upload

--- converte_metros.py
+++ converte_metros.py
@@ -5,5 +5,7 @@
  4. exibir na saída a quantidade de metros, e os valores convertidos, formatados
  para 0 números após a vírgula. '''
 
+metros = float(input('Insira o valor em metros: '))
 
+print('{} metros em centímetros é igual a: {:.2f} \nem milímetros é igual a: {:.2f}'.format(metros, metros * 100, metros * 1000))
 
