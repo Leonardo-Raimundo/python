b68c0b06b3a150f14a416b5169ae67b9ecfe8559
Leonardo Raimundo <90846755+Leonardo-Raimundo@users.noreply.github.com>
2023-08-11 11:46:57 -0300

    Add files via upload

---
+++ somatorio.py
@@ -0,0 +1,13 @@
+x1 = 1
+x2 = 3
+x3 = 5
+y1 = 2
+y2 = 4
+y3 = 7
+
+somatorio_a = x1 + x2 + x3
+somatorio_b = y1 + y2 + y3
+somatorio_c = (x1 + y1) + (x2 + y2) + (x3 + y3)
+somatorio_d = somatorio_a + somatorio_b
+
+print('A = {} \nB = {} \nC = {} \nD = {}'.format(somatorio_a, somatorio_b, somatorio_c, somatorio_d))
