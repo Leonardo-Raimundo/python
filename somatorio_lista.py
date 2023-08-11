e99965a27842fdc84d1446df5546a76e341253a8
Leonardo Raimundo <90846755+Leonardo-Raimundo@users.noreply.github.com>
2023-08-11 11:49:39 -0300

    Add files via upload

---
+++ somatorio_lista.py
@@ -0,0 +1,9 @@
+x = [1, 3, 5]
+y = [2, 4, 7]
+
+somatorio_a = sum(x)
+somatorio_b = sum(y)
+somatorio_c = (x[0] + y[0]) + (x[1] + y[1]) + (x[2] + y[2])
+somatorio_d = sum(x) + sum(y)
+
+print('A = {} \nB = {} \nC = {} \nD = {}'.format(somatorio_a, somatorio_b, somatorio_c, somatorio_d))
